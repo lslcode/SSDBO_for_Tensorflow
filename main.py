23fd32649f9bdaf70ad755bc14285ecf7e970311
lslcode <33273994+lslcode@users.noreply.github.com>
2017-12-19 16:37:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-date: 2017/12/01
+date: 2017/12/10
 author: lslcode [jasonli8848@qq.com]
 """
 
@@ -24,21 +24,24 @@ def testing():
         saver = tf.train.Saver(var_list=tf.trainable_variables())
         if os.path.exists('./session_params/session.ckpt.index') :
             saver.restore(sess, './session_params/session.ckpt')
-            image, actual,file_list = get_traindata_voc2012(1)
-            pred_class, pred_location = ssdbo_model.run(image,None)
+            image, actual,file_list = get_traindata_voc2007(1)
+            pred_class, pred_class_val, pred_location = ssdbo_model.run(image,None)
             print('file_list:' + str(file_list))
              
             for index, act in zip(range(len(image)), actual):
                 for a in act :
                     print('【img-'+str(index)+' actual】:' + str(a))
-                    '''
+                '''    
                     for p_i, p_class in zip(range(len(pred_class[index])),pred_class[index]):
                         if(a[4]==p_class):
                             print('pred_class:' + str(p_class))
+                            print('pred_class_val:' + str(pred_class_val[index][p_i]))
                             print('pred_location:' + str(pred_location[index][p_i]))
-                    '''
+                '''
                 print('pred_class:' + str(pred_class[index]))
+                print('pred_class_val:' + str(pred_class_val[index]))
                 print('pred_location:' + str(pred_location[index]))
+                
                 
               
         else:
@@ -65,15 +68,37 @@ def training():
         print('\nStart Training')
        
         min_loss_class = 10000000.
-        while(min_loss_class > 1 and running_count < 100000):
+        while(min_loss_class > 0.0001 and running_count < 100000):
             running_count += 1
             
-            train_data, actual_data,_ = get_traindata_voc2012(batch_size)
+            train_data, actual_data,file_list = get_traindata_voc2007(batch_size)
+            #print('file_list:' + str(file_list))
+            #print('actual_data:' + str(actual_data))
             if len(train_data) > 0:
-                loss_all, f_class = ssdbo_model.run(train_data, actual_data)
+                loss_all, f_class, softmax_cross, h_positives = ssdbo_model.run(train_data, actual_data)
                 loss_all = np.sum(loss_all)
                 if min_loss_class > loss_all:
                     min_loss_class = loss_all
+
+                #print('h_positives :【'+str(np.sum(h_positives))+'|'+str(np.amax(h_positives))+'|'+str(np.amin(h_positives))+'|'+str(np.mean(h_positives))+'】')
+                #print('softmax_cross :【'+str(np.sum(softmax_cross))+'|'+str(np.amax(softmax_cross))+'|'+str(np.amin(softmax_cross))+'|'+str(np.mean(softmax_cross))+'】')
+                
+                count = 0;
+                for h_i in range(len(train_data)):
+                    for i_i in range(ssdbo_model.all_default_boxs_len):
+                        if h_positives[h_i][i_i]==1:
+                            print('softmax_cross_val :【'+str(softmax_cross[h_i][i_i])+'】【'+str(h_positives[h_i][i_i])+'】')
+                            count+=1
+                            if count>15:break
+                    if count>15:break    
+                count = 0;
+                for h_i in range(len(train_data)):
+                    for i_i in range(ssdbo_model.all_default_boxs_len):
+                        if h_positives[h_i][i_i]==0:
+                            print('softmax_cross_val :【'+str(softmax_cross[h_i][i_i])+'】')
+                            count+=1
+                            if count>15:break
+                    if count>15:break 
                
                 print('Running:【' + str(running_count) + '】|Loss All:【'+str(min_loss_class)+'|'+str(loss_all) + '】|pred_class:【'+ str(np.sum(f_class))+'|'+str(np.amax(f_class))+'|'+ str(np.amin(f_class))+'|'+ str(np.mean(f_class)) + '】')
                 
@@ -93,15 +118,15 @@ def training():
     print('End Training')
     
 '''
-获取voc2012训练图片数据
+获取voc2007训练图片数据
 train_data：训练批次图像，格式[None,width,height,3]
-actual_data：图像标注数据，格式[None,[None,top_x,top_y,width,height,lable]]
+actual_data：图像标注数据，格式[None,[None,center_x,center_y,width,height,lable]]
 '''
-file_name_list = os.listdir('./train_datasets/voc2012/JPEGImages/')
+file_name_list = os.listdir('./train_datasets/voc2007/JPEGImages/')
 lable_arr = ['background','aeroplane','bicycle','bird','boat','bottle','bus','car','cat','chair','cow','diningtable','dog','horse','motorbike','person','pottedplant','sheep','sofa','train','tvmonitor']
 # 图像白化，格式:[R,G,B]
-whitened_RGB_mean = [123.68, 116.78, 103.94]
-def get_traindata_voc2012(batch_size):
+#whitened_RGB_mean = [123.68, 116.78, 103.94]
+def get_traindata_voc2007(batch_size):
     def get_actual_data_from_xml(xml_path):
         actual_item = []
         try:
@@ -116,8 +141,8 @@ def get_traindata_voc2012(batch_size):
                 y_min = float(bndbox.find('ymin').text.strip())
                 x_max = float(bndbox.find('xmax').text.strip())
                 y_max = float(bndbox.find('ymax').text.strip())
-                # 位置数据用比例来表示，格式[top_x,top_y,width,height,lable]
-                actual_item.append([(x_min / img_width), (y_min / img_height), ((x_max - x_min) / img_width), ((y_max - y_min) / img_height), lable])
+                # 位置数据用比例来表示，格式[center_x,center_y,width,height,lable]
+                actual_item.append([((x_min + x_max)/2/img_width), ((y_min + y_max)/2/img_height), ((x_max - x_min) / img_width), ((y_max - y_min) / img_height), lable])
             return actual_item  
         except:
             return None
@@ -128,8 +153,8 @@ def get_traindata_voc2012(batch_size):
     file_list = random.sample(file_name_list, batch_size)
     
     for f_name in file_list :
-        img_path = './train_datasets/voc2012/JPEGImages/' + f_name
-        xml_path = './train_datasets/voc2012/Annotations/' + f_name.replace('.jpg','.xml')
+        img_path = './train_datasets/voc2007/JPEGImages/' + f_name
+        xml_path = './train_datasets/voc2007/Annotations/' + f_name.replace('.jpg','.xml')
         if os.path.splitext(img_path)[1].lower() == '.jpg' :
             actual_item = get_actual_data_from_xml(xml_path)
             if actual_item != None :
@@ -140,7 +165,7 @@ def get_traindata_voc2012(batch_size):
             img = skimage.io.imread(img_path)
             img = skimage.transform.resize(img, (300, 300))
             # 图像白化预处理
-            img = img - whitened_RGB_mean
+            #img = img - whitened_RGB_mean
             train_data.append(img)
             
     return train_data, actual_data,file_list
